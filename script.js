e4e7e2b374b4e77971bd197c5b0ebdd5d304a40c
Aishi-Jain <162431264+Aishi-Jain@users.noreply.github.com>
2024-06-14 16:47:05 +0530

    comment out all the js code to create a simple cart quantity project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let variable1 = 3;
+/* let variable1 = 3;
 console.log(variable1);
 
 let calculation = 2 + 2;
@@ -15,4 +15,4 @@ variable1 = 5;
 console.log(variable1);
 
 variable1 = variable1 + 1;
-console.log(variable1);
+console.log(variable1); */
