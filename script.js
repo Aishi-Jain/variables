c322c0164449446e82afb72e648006c82bdd8db8
Aishi-Jain <162431264+Aishi-Jain@users.noreply.github.com>
2024-06-14 16:28:47 +0530

    placing a variable instead of a value

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let variable1 = 3;
 console.log(variable1);
+
+let calculation = 2 + 2;
+console.log(calculation);
+console.log(calculation + 12);
+
+let result = calculation + variable1;
+console.log(result);
